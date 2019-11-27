1d6ed5603308594ca7b9cf74225cf7f81b6cc2b8
Pat Brown <ptrckbrown94@gmail.com>
2019-11-27 16:30:34 -0600

    added 4 functions for each check box and the onclick for search button

--- Assets/Javascript/edamam.js
+++ Assets/Javascript/edamam.js
@@ -4,8 +4,19 @@ function edamam() {
     const appKey = "87537f8acf10951583c43ab5434ae1d5";
     const userInput = "taco";
     const edamamURL = "https://api.edamam.com/search?q="+userInput+"&app_id="+appId+"&app_key="+appKey;
+    const searchButtonEl= document.getElementById("search-button");
+    
+   
+    
     
 
+
+    searchButtonEl.addEventListener("click", function (){
+        console.log("click")
+
+     
+    
+    });
 
     axios.get(edamamURL).then(function(response){
         console.log(edamamURL);
@@ -13,3 +24,42 @@ function edamam() {
     })
 }
 edamam();
+
+// got this from w3schools and Stack Overflow
+
+const glutenFree = document.querySelector("gluten-freeCheckBox");   
+glutenFree.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
+});
+
+const dairyFree = document.querySelector("dairy-freeCheckBox");
+dairyFree.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
+});
+
+const vegan =  document.querySelector("veganCheckBox");
+vegan.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
+});
+
+
+const vegetarian = document.querySelector("vegetarianCheckBox");
+vegetarian.addEventListener( 'change', function() {
+    if(this.checked) {
+        // Checkbox is checked..
+    } else {
+        // Checkbox is not checked..
+    }
+});
